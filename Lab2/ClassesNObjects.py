b00b5174dfb154b72f4096c4684fc046fd0f0058
MarcoAntonioMontez <marco.montez.lis@gmail.cm>
2018-10-02 09:03:20 -0700

    ClassesNObjects Example

--- Lab2/ClassesNObjects.py
+++ Lab2/ClassesNObjects.py
@@ -1,1 +1,26 @@
-## empty
+class rpnCalculator:
+    stack = []
+
+    def pushValue (self,value):
+        self.stack.append(value)
+
+
+    def popValue (self):
+        value = self.stack.pop()
+        return value
+
+
+    def add (self):
+        value1 = self.popValue()
+        value2 = self.popValue()
+        sum = value1+value2
+        self.pushValue(sum)
+
+calculator = rpnCalculator();
+calculator.pushValue(1)
+calculator.pushValue(2)
+
+calculator.add()
+value=calculator.popValue()
+print(value)
+
