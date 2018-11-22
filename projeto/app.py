bedd4abbf6f23d02c956da75c761f6798edcd496
MarcoAntonioMontez <marco.montez.lis@gmail.cm>
2018-11-22 11:29:40 -0800

    Create Flask Server with user

---
+++ projeto/app.py
@@ -0,0 +1,70 @@
+from flask import Flask,jsonify,abort,make_response,request
+
+app = Flask(__name__)
+
+users = [
+    {
+        'id': 'ist178508',
+        'latitude': "10.0000",
+        'longitude': 10.0001
+    },
+    {
+        'id': 'ist179021',
+        'latitude': "10.0000",
+        'longitude': 10.0002
+    }
+]
+
+@app.route('/todo/api/v1.0/users', methods=['GET'])
+def get_users():
+    return jsonify({'users': users})
+
+
+@app.route('/todo/api/v1.0/users/<string:user_id>', methods=['GET'])
+def get_user(user_id):
+    user = [user for user in users if user['id'] == user_id]
+    if len(user) == 0:
+        abort(404)
+    return jsonify({'task': user[0]})
+
+
+
+@app.route('/todo/api/v1.0/users', methods=['POST'])
+def create_task():
+    if not request.json or not 'id' in request.json or not 'latitude' in request.json or not 'longitude' in request.json:
+        abort(400)
+    user = {
+        'id': request.json['id'],
+        'latitude': request.json['latitude'],
+        'longitude': request.json['longitude']
+    }
+
+    for existing_user in users:
+        if user["id"] == existing_user["id"]:
+            existing_user["latitude"] = user["latitude"]
+            existing_user["longitude"] = user["longitude"]
+            return jsonify({'existing_user': existing_user}), 201
+
+    users.append(user)
+    return jsonify({'user': user}), 201
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+@app.errorhandler(400)
+def not_found(error):
+    return make_response(jsonify({'error': 'Bad request'}), 400)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+#Get
+#  curl -i http://localhost:5000/todo/api/v1.0/users/ist178508
+
+
+#POST
+#curl -i -H "Content-Type: application/json" -X POST -d '{"id":"ist169699", "title":"Read a book"}' http://localhost:5000/todo/api/v1.0/users
--- projeto/main.py
+++ projeto/main.py
@@ -1,1 +1,26 @@
-print('Hello World')
+users = [
+    {
+        'id': 'ist178508',
+        'latitude': "10.0000",
+        'longitude': 10.0001
+    },
+    {
+        'id': 'ist179021',
+        'latitude': "10.0000",
+        'longitude': 10.0002
+    }
+]
+
+user = {
+        'id': 'ist178508',
+        'latitude': "10.0000",
+        'longitude': 20.00
+    }
+
+
+for existing_user in users:
+    if user["id"]==existing_user["id"]:
+        existing_user["latitude"]=user["latitude"]
+        existing_user["longitude"]=user["longitude"]
+        break
+print(users)
