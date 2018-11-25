33a5f94f8febc00c9fca24247c862f16a13f6c7d
joaoalves <jmalves5@hotmail.com>
2018-11-25 16:48:16 +0000

    changed log class

---
+++ projeto/log.py
@@ -0,0 +1,10 @@
+class Log:
+    def __init__(self, type, timestamp):
+        self.type = type
+        self.timestamp = timestamp
+
+    def get_type(self):
+        return self.type
+
+    def get_timestamp(self):
+        return self.timestamp
